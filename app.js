e460a764c9d5ba2a9ecb573ba6be199805709702
sagar <sagarramrudri@gmail.com>
2024-07-01 07:28:37 +0545

    create edit delete added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const path  = require("path");
 const Listing = require("./models/listing.js");
+const methodOverride = require("method-override")
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -17,6 +18,8 @@ async function main(){
 app.set("view engine" , "ejs");
 app.set("views" ,path.join(__dirname , "views"));
 app.use(express.urlencoded({extended : true}));
+
+app.use(methodOverride("_method"));
 
 // app.get("/testListing" , async (req,res) => {
 //     let sampleListing = new Listing({
@@ -38,14 +41,50 @@ app.get("/listings", async (req,res) => {
     res.render("listings/index.ejs" , { allListings});
 });
 
-// Show Route 
+//New Route
+app.get("/listings/new" ,(req,res) => {
+    res.render("listings/new.ejs");
+ })
 
+// Show Route 
 app.get("/listings/:id" , async (req,res) => {
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", {listing})
 
 });
+
+// create Route 
+app.post("/listings" , async (req,res) => {
+//    let {title, description, image, price, location , country} = req.body;
+    // let listing = req.body.listing;
+    // console.log(listing);
+   const newListing =  new Listing(req.body.listing);
+   await newListing.save();
+   res.redirect("/listings");
+});
+
+app.get("/listings/:id/edit" , async (req,res) =>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs" , {listing} )
+})
+
+// update Route
+
+app.put("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+})
+
+//Delete Route
+app.delete("/listings/:id" , async (req,res) => {
+  let {id} = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
+})
 
 app.get("/" , (req,res) => {
     res.send("Hi, I am root");
