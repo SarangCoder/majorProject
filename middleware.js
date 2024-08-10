54fbbffe4c4a25a055b1411db0e6642813f58b80
sagar <sagarramrudri@gmail.com>
2024-08-10 13:05:59 +0545

    login logout all features are added

---
+++ middleware.js
@@ -0,0 +1,19 @@
+module.exports.isLoggedIn = (req, res, next) => {
+    console.log(req.path , "..." , req.originalUrl);
+    
+    if (!req.isAuthenticated()) {
+        //redirectUrl save
+        req.session.redirectUrl = req.originalUrl;
+        req.flash("error", "you must be logged in to create listing!");
+        return res.redirect("/login");
+    }
+    next();
+}
+
+
+module.exports.saveRedirectUrl = (req,res, next) => {
+    if(req.session.redirectUrl){
+        res.locals.redirectUrl = req.session.redirectUrl;
+    }
+    next();
+}
--- routes/user.js
+++ routes/user.js
@@ -3,33 +3,55 @@ const router = express.Router();
 const User = require("../models/user");
 const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware");
 
 
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
 });
 
-router.post("/signup", wrapAsync(async (req, res) => {
+router.post("/signup", wrapAsync(async (req, res, next) => {
     try {
         let { username, email, password } = req.body;
         const newUser = new User({ email, username });
         const registeredUser = await User.register(newUser, password);
         console.log(registeredUser);
-        req.flash("success", "Welcome to Wanderlust");
-        res.redirect("/listings")
+        req.login(registeredUser, (err) => {
+            if(err){
+             return next(err);
+            }
+
+            req.flash("success", "Welcome to Wanderlust");
+            res.redirect("/listings")
+
+        })
+        
     } catch (e) {
         req.flash("error", "user is already exist");
         res.redirect("/signup");
     }
 }));
 
+
+//for logout
+router.get("/logout" , (req,res , next) => {
+    req.logOut((err) => {
+        if(err){
+         return next();
+        }
+        req.flash("success" , "You are logged out !");
+        res.redirect("/listings");
+    })
+});
+
 router.get("/login", (req, res) => {
     res.render("users/login.ejs");
 });
 
-router.post("/login", passport.authenticate("local", { failureRedirect: '/login', failureFlash: true, }), async (req, res) => {
+router.post("/login", saveRedirectUrl, passport.authenticate("local", { failureRedirect: '/login', failureFlash: true, }), async (req, res) => {
     req.flash("success" , "Welcome back to Wanderlust!");
-    res.redirect("/listings");
-})
+    let redirectUrl = res.locals.redirectUrl || "/listings"
+    res.redirect(redirectUrl);
+});
 
 module.exports = router;
