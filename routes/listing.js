d07eaa42cb7e6c9dceeb72af33daaacfe13935f8
sagar <sagarramrudri@gmail.com>
2024-09-05 15:34:59 +0545

    image features is not added

--- routes/listing.js
+++ routes/listing.js
@@ -8,13 +8,18 @@ const listingSchema = require("../schema.js")
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js")
 const listingController = require("../controllers/listing.js");
+const{storage } = require("../cloudConfig.js");
 
+//for uploading the file
+const multer  = require('multer');
+const upload = multer({storage });
 
 router.route("/")
     //Index Route
     .get(wrapAsync(listingController.index))
     // create Route 
-    .post(isLoggedIn, validateListing, wrapAsync(listingController.createListing));
+    .post(isLoggedIn, validateListing,upload.single("listing[image]") , wrapAsync(listingController.createListing));
+   
 
     //New Route
 router.get("/new", isLoggedIn, listingController.renderNewForm)
